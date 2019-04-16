b3b4211b35ca4a1499867d0e8dd8d4f3ffc033d0
Roman_Moskvin <moskvin.roman2018@yandex.ru>
2019-04-17 02:01:30 +0700

    Обновлен файл gitignore

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import './css/normalize.css'
+import './scss/main.scss'
+
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import 'vue'
+// import Vue from 'vue'
+
+
